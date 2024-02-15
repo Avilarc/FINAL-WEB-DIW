2c5e96fd9bfd2ac1296ed4da5fe4ed77a16d3693
Avilarc <nonoavawork@gmail.com>
2024-02-15 21:38:26 +0100

    solucionando

--- fuente/scripts/builds.js
+++ fuente/scripts/builds.js
@@ -1,4 +1,6 @@
 let botonFiltro = document.getElementsByClassName("filter-list-item");
+let builds = document.getElemenQuuerySelectorAll(".build");
+
 
 for(let i = 0; i < botonFiltro.length; i++) {
     botonFiltro[i].addEventListener("click", function() {
@@ -8,5 +10,18 @@ for(let i = 0; i < botonFiltro.length; i++) {
         }
         // Agregar la clase 'active' al elemento clickeado
         this.classList.add("active");
+        let categoriaSeleccionada = this.getAtrribute("categoria");
+
+        builds.forEach(build => {
+            build.style.display = "none";
+
+        });
+
+        builds.forEach(build => {
+            if(build.getAttribute("categoria") == categoriaSeleccionada || categoriaSeleccionada == "all") {
+                build.style.display = "block";
+            }
+        });
+
     });
 }
